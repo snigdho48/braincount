fe86b78ca2979da00182e5a9a1816b953ffac482
snigdho48 <www.atik48@gmail.com>
2025-04-15 05:34:18 +0600

    add patch

--- lib/app/data/constants.dart
+++ lib/app/data/constants.dart
@@ -1,3 +1,3 @@
 const String url = 'https://bc.reachableads.com';
 const String url2 = 'https://ce10-103-49-202-229.ngrok-free.app';
-const String baseUrl = '$url2/api/';
+const String baseUrl = '$url/api/';
